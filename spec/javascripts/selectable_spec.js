b0b319411765088cabefa7b58a453da2a2ab5821
ragaskar <ragaskar@gmail.com>
2010-01-02 17:33:19 -0800

    Added selected, unselected, update events

--- spec/javascripts/selectable_spec.js
+++ spec/javascripts/selectable_spec.js
@@ -1,5 +1,5 @@
 describe('Selectable', function() {
-  var fixture;
+  var fixture, select1, select2;
   beforeEach(function() {
     $('#jasmine_content').html('');
     fixture = $('<ul id="container">' +
@@ -7,6 +7,8 @@ describe('Selectable', function() {
                 '<li id="select2" class="select">select2</li>' +
                 '</ul>');
     $('#jasmine_content').html(fixture);
+    select1 = $('#select1')[0];
+    select2 = $('#select2')[0];
   });
 
   it('should add a class to selected items', function() {
@@ -37,15 +39,45 @@ describe('Selectable', function() {
     $('#select2').click();
     selected = $('.select').selectable().selected();
     expect(selected.length).toEqual(2);
-    expect(selected[0]).toEqual($('#select1')[0]);
-    expect(selected[1]).toEqual($('#select2')[0]);
+    expect(selected[0]).toEqual(select1);
+    expect(selected[1]).toEqual(select2);
 
     $('#select1').click();
     selected = $('.select').selectable().selected();
     expect(selected.length).toEqual(1);
-    expect(selected[0]).toEqual($('#select2')[0]);
+    expect(selected[0]).toEqual(select2);
+  });
 
+  it("should fire an event on selection", function() {
+    $('.select').selectable();
+    var selectedSpy = jasmine.createSpy('selectedSpy');
+    $('.select').bind(jQuery.fn.selectable.SELECTED, selectedSpy);
+    $('#select1').click();
+    expect(selectedSpy).wasCalled();
+    expect(selectedSpy.mostRecentCall.args[0].target).toEqual(select1);
+  });
 
+  it("should fire an event on de-selection", function() {
+    $('.select').selectable();
+    var selectedSpy = jasmine.createSpy('selectedSpy');
+    $('.select').bind(jQuery.fn.selectable.UNSELECTED, selectedSpy);
+    $('#select1').click();
+    expect(selectedSpy).wasNotCalled();
+    $('#select1').click();
+    expect(selectedSpy).wasCalled();
+    expect(selectedSpy.mostRecentCall.args[0].target).toEqual(select1);
+  });
+
+  it("should fire an event on selection OR de-selection", function() {
+    $('.select').selectable();
+    var selectedSpy = jasmine.createSpy('selectedSpy');
+    $('.select').bind(jQuery.fn.selectable.UPDATE, selectedSpy);
+    $('#select1').click();
+    expect(selectedSpy).wasCalled();
+    expect(selectedSpy.mostRecentCall.args[0].target).toEqual(select1);
+    $('#select1').click();
+    expect(selectedSpy).wasCalled();
+    expect(selectedSpy.mostRecentCall.args[0].target).toEqual(select1);
   });
 
 });
